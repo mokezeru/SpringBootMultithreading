3e5384131b2ad4882aa21628508d7317472f5995
P2883280 <C-MequannintMunye.Zeru@charter.com>
2019-08-07 15:17:58 -0500

    Initial Commit

---
+++ springAsyncService/src/main/java/com/charter/springAsyncService/controller/AsyncController.java
@@ -0,0 +1,50 @@
+package com.charter.springAsyncService.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import com.charter.springAsyncService.model.EmployeeAddresses;
+import com.charter.springAsyncService.model.EmployeeNames;
+import com.charter.springAsyncService.model.EmployeePhone;
+import com.charter.springAsyncService.service.AsyncService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AsyncController {
+
+	private static Logger log = LoggerFactory.getLogger(AsyncController.class);
+
+	@Autowired
+	private AsyncService service;
+
+	@RequestMapping(value = "/testAsynch", method = RequestMethod.GET)
+	public List<Object> testAsynch() throws InterruptedException, ExecutionException
+	{
+		log.info("testAsynch Start");
+
+		CompletableFuture<EmployeeAddresses> employeeAddress = service.getEmployeeAddress();
+		CompletableFuture<EmployeeNames> employeeName = service.getEmployeeName();
+		CompletableFuture<EmployeePhone> employeePhone = service.getEmployeePhone();
+
+		// Wait until they are all done
+		CompletableFuture.allOf(employeeAddress, employeeName, employeePhone).join();
+		//Build objects to return
+		List<Object> objects = new ArrayList<>();
+		objects.add(employeeName.get());
+		objects.add(employeeAddress.get());
+		objects.add(employeePhone.get());
+		
+		log.info("EmployeeAddress--> " + employeeAddress.get());
+		log.info("EmployeeName--> " + employeeName.get());
+		log.info("EmployeePhone--> " + employeePhone.get());
+		return objects;
+	}
+}
---
+++ springMultiThreading/src/main/java/com/charter/springMultiThreading/SpringMultiThreadingApplication.java
@@ -0,0 +1,13 @@
+package com.charter.springMultiThreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiThreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMultiThreadingApplication.class, args);
+	}
+
+}
